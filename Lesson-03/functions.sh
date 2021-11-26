44794138cd4d417138772bd30439ff172160f4cd
Dmitriy Rusakov <rusd80@gmail.com>
2021-11-26 15:14:02 +0300

    update script.sh

--- Lesson-03/functions.sh
+++ Lesson-03/functions.sh
@@ -1,17 +1,20 @@
 #!/bin/env bash
 
 function get_ips(){
-  local res=$(ss -tunap | awk -v pat=$1 '$0 ~pat {print $6}' | uniq -c | grep -oP '(\d+\.){3}\d+' | sort | tail -n$2 | uniq -c)
+  local res=$(ss -tunap | awk -v pat=$1 '$0 ~pat {print $6}' | sort | uniq -c | grep -oP '(\d+\.){3}\d+' )
   echo $res
 }
 
 function get_info(){
-  local res=$(whois $1 | awk -F':' '/^Organization|^role/ {print $2}' )
+  [ $mode == 'normal' ] && local res=$(whois $1 | awk -F':' '/^Organization|^role/ {print $2}' | cut -d'(' -f1 )
+  [ $mode == 'wide' ] && local res=$(whois $1 | awk -F':' '/^Organization|organization|^OrgName|org-name|ddress|^OrgTechEmail|mailbox/ {print $2}' ORS="  |  "  )
   echo $res
 }
 
-function get_info_wide(){
-  local res=$(whois $1 | awk -F':' '/rganization|role|ountry|ddress|^OrgTechEmail|mailbox/ {print $2}' ORS=", "  )
-  echo $res
+function print_frame(){
+  separator_line="+--------------------|--------------------------------------------------+"
+  echo $separator_line
+  printf "%-70s | \n" \
+                 "| Number of connects |  Organization         [ Info ]        "
+  echo $separator_line
 }
-
--- Lesson-03/script.sh
+++ Lesson-03/script.sh
@@ -17,11 +17,13 @@ $ ./script.sh -h or --help'
 
 [ -z "$1" ] && echo "Error: process name or PID required" && exit 1
 [ -z "$2" ] && echo "Error: number of outputs required" && exit 1
+[ -z "$3" ] && echo "Error: mode required" && exit 1
 
 [ -z "$(which ss)" ] && err "Please install iproute2 package." && exit 2
 [ -z "$(which whois)" ] && err "Please install whois package." && exit 2
 
-[ "$3" == '-w' ] && mode='wide' || mode='normal'
+[ "$3" == '-w' ] && mode='wide'
+[ "$3" == '-n' ] && mode='normal'
 
 ip_list=$(get_ips $1 $2)
 
@@ -29,14 +31,11 @@ ip_list=$(get_ips $1 $2)
 
 for ip in $ip_list
 do
-  [ $mode == 'normal' ] && echo $(get_info $ip)
-  [ $mode == 'wide' ] && echo $(get_info_wide $ip)
+  info=$(get_info $ip)
+  [[ -n "$info" && -n "$org_list" ]] && org_list+=$'\n'
+  [ -n "$info" ] && org_list+=$'\t\t'$(echo $info)
 done
 
+print_frame
 
-
-
-
-
-
-
+echo -e "$org_list" |sort | uniq -c | sort -nr | head -n$2

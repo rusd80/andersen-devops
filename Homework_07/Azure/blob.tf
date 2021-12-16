55c510fbb40a7b036aff057d9a8b41bd01c0999c
Dmitriy Rusakov <rusd80@gmail.com>
2021-12-16 18:41:58 +0300

    add blob and index.html upload

--- Homework_07/Azure/blob.tf
+++ Homework_07/Azure/blob.tf
@@ -1,8 +1,8 @@
-/*
+
 resource "azurerm_storage_account" "myacc" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  name                     = "examplestoracc8011"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -20,4 +20,3 @@ resource "azurerm_storage_blob" "myblob" {
   type                   = "Block"
   source                 = "index.html"
 }
-*/
--- Homework_07/Azure/script.sh
+++ Homework_07/Azure/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-det -y update
 sudo apt-get install -y nginx
+azcopy copy "https://examplestoracc8011.blob.core.windows.net/content/index.html" "/usr/share/nginx/html/index.html"
 sudo systemctl start nginx
 sudo systemctl enable nginx
